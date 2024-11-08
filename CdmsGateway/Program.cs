58ccca4f4388e6f9e3429beb44cec958c8c418a2
Ian Shimmings <ian.shimmings@hotmail.com>
2024-11-08 12:54:21 +0000

    Added OTel to stub and changed OTel names

--- CdmsGateway/Program.cs
+++ CdmsGateway/Program.cs
@@ -18,73 +18,72 @@ return;
 [ExcludeFromCodeCoverage]
 static WebApplication CreateWebApplication(string[] args)
 {
-   var builder = WebApplication.CreateBuilder(args);
+    var builder = WebApplication.CreateBuilder(args);
 
-   ConfigureWebApplication(builder);
+    ConfigureWebApplication(builder);
 
-   var app = builder.BuildWebApplication();
+    var app = builder.BuildWebApplication();
 
-   return app;
+    return app;
 }
 
 [ExcludeFromCodeCoverage]
 static void ConfigureWebApplication(WebApplicationBuilder builder)
 {
-   builder.Configuration.AddEnvironmentVariables();
-   builder.Configuration.AddIniFile("Properties/local.env", true);
+    builder.Configuration.AddEnvironmentVariables();
+    builder.Configuration.AddIniFile("Properties/local.env", true);
 
-   //OTEL
+    //OTEL
+    builder.Services.AddOpenTelemetry()
+        .WithMetrics(metrics =>
+        {
+            metrics.AddRuntimeInstrumentation()
+                .AddMeter(
+                    "Microsoft.AspNetCore.Hosting",
+                    "Microsoft.AspNetCore.Server.Kestrel",
+                    "System.Net.Http");
+        })
+        .WithTracing(tracing =>
+        {
+            tracing.AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation();
+        })
+        .UseOtlpExporter();
 
-   builder.Services.AddOpenTelemetry()
-       .WithMetrics(metrics =>
-       {
-           metrics.AddRuntimeInstrumentation()
-               .AddMeter(
-                   "Microsoft.AspNetCore.Hosting",
-                   "Microsoft.AspNetCore.Server.Kestrel",
-                   "System.Net.Http");
-       })
-       .WithTracing(tracing =>
-       {
-           tracing.AddAspNetCoreInstrumentation()
-               .AddHttpClientInstrumentation();
-       })
-       .UseOtlpExporter();
+    var logger = ConfigureLogging(builder);
 
-var logger = ConfigureLogging(builder);
+    // Load certificates into Trust Store - Note must happen before Mongo and Http client connections
+    builder.Services.AddCustomTrustStore(logger);
 
-   // Load certificates into Trust Store - Note must happen before Mongo and Http client connections
-   builder.Services.AddCustomTrustStore(logger);
+    ConfigureMongoDb(builder);
 
-   ConfigureMongoDb(builder);
+    builder.ConfigureEndpoints();
 
-   builder.ConfigureEndpoints();
-
-   builder.AddServices(logger);
+    builder.AddServices(logger);
 }
 
 [ExcludeFromCodeCoverage]
 static Logger ConfigureLogging(WebApplicationBuilder builder)
 {
-   builder.Logging.ClearProviders();
-   var logger = new LoggerConfiguration()
-       .ReadFrom.Configuration(builder.Configuration)
-       .Enrich.With<LogLevelMapper>()
-       .WriteTo.OpenTelemetry(options =>
-       {
-           options.Endpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
-           options.ResourceAttributes.Add("service.name", "Cdms-Gatway");
-       })
-       .CreateLogger();
-   builder.Logging.AddSerilog(logger);
-   logger.Information("Starting application");
-   return logger;
+    builder.Logging.ClearProviders();
+    var logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .Enrich.With<LogLevelMapper>()
+        .WriteTo.OpenTelemetry(options =>
+        {
+            options.Endpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
+            options.ResourceAttributes.Add("service.name", "Cdms-Gatway");
+        })
+        .CreateLogger();
+    builder.Logging.AddSerilog(logger);
+    logger.Information("Starting application");
+    return logger;
 }
 
 [ExcludeFromCodeCoverage]
 static void ConfigureMongoDb(WebApplicationBuilder builder)
 {
-   builder.Services.AddSingleton<IMongoDbClientFactory>(_ =>
-       new MongoDbClientFactory(builder.Configuration.GetValue<string>("Mongo:DatabaseUri")!,
-           builder.Configuration.GetValue<string>("Mongo:DatabaseName")!));
+    builder.Services.AddSingleton<IMongoDbClientFactory>(_ =>
+        new MongoDbClientFactory(builder.Configuration.GetValue<string>("Mongo:DatabaseUri")!,
+            builder.Configuration.GetValue<string>("Mongo:DatabaseName")!));
 }
