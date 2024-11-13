6a3c8ce0c82378277c33802f03ae26fc9759178f
Ian Shimmings <ian.shimmings@hotmail.com>
2024-11-13 15:39:51 +0000

    Add trace routing

--- CdmsGateway/Services/CheckRoutes.cs
+++ CdmsGateway/Services/CheckRoutes.cs
@@ -1,5 +1,7 @@
+using System.Diagnostics;
 using System.Net;
 using System.Net.Mime;
+using System.Runtime.InteropServices;
 using System.Text;
 using CdmsGateway.Services.Routing;
 using CdmsGateway.Utils.Http;
@@ -19,14 +21,43 @@ public class CheckRoutes(IMessageRoutes messageRoutes, IHttpClientFactory client
         var client = clientFactory.CreateClient(Proxy.ProxyClient);
         var request = new HttpRequestMessage(new HttpMethod(healthUrl.Method), healthUrl.Url);
         var response = await client.SendAsync(request);
-        return $"{healthUrl.Name} - {healthUrl.Method} {healthUrl.Url} - {response.StatusCode}";
+        var traceRoute = TraceRoute(healthUrl.Url);
+        return $"{healthUrl.Name} - {healthUrl.Method} {healthUrl.Url} - {response.StatusCode}\r{traceRoute}";
     }
 
-    private async Task SetResponse(HttpResponse response, string[] results)
+    private static async Task SetResponse(HttpResponse response, string[] results)
     {
-        await response.BodyWriter.WriteAsync(new ReadOnlyMemory<byte>(Encoding.UTF8.GetBytes(string.Join("\r", results))));
-        response.ContentType = MediaTypeNames.Text.Plain;
         response.StatusCode = (int)HttpStatusCode.OK;
+        response.ContentType = MediaTypeNames.Text.Plain;
+        await response.BodyWriter.WriteAsync(new ReadOnlyMemory<byte>(Encoding.UTF8.GetBytes(string.Join("\r", results))));
     }
 
+    private static string TraceRoute(string url)
+    {
+        var uri = new Uri(url);
+        return RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+            ? RunProcess("cmd.exe", $"/c tracert {uri.Host}")
+                .Replace("\r\n", "\r")
+                .Replace("\rTracing", "Tracing")
+                .Replace("]\rover", "] over")
+                .Replace("\r\r", "\r")
+                .Replace("Trace complete.\r", "")
+            : RunProcess("/bin/bash", $"-c \"traceroute {uri.Host}\"");
+    }
+
+    private static string RunProcess(string fileName, string arguments)
+    {
+        var startInfo = new ProcessStartInfo()
+        {
+            FileName = fileName,
+            Arguments = arguments,
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+            
+        using var process = Process.Start(startInfo);
+        using var reader = process?.StandardOutput;
+        return reader?.ReadToEnd() ?? string.Empty;
+    }
 }
