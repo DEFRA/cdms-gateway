5d41e888bd2704242e98dee3ea757a309081e24d
Ian Shimmings <ian.shimmings@hotmail.com>
2024-11-13 17:49:23 +0000

    Made rout checking more resilient

--- CdmsGateway/Services/Checking/CheckRoutes.cs
+++ CdmsGateway/Services/Checking/CheckRoutes.cs
@@ -16,20 +16,32 @@ public class CheckRoutes(IMessageRoutes messageRoutes, IHttpClientFactory client
 
     private async Task<string> Check(HealthUrl healthUrl)
     {
-        var client = clientFactory.CreateClient(Proxy.ProxyClient);
-        var request = new HttpRequestMessage(new HttpMethod(healthUrl.Method), healthUrl.Url);
-        var response = await client.SendAsync(request);
-        
-        var hostname = new Uri(healthUrl.Url).Host;
+        string statusCode;
+        try
+        {
+            var client = clientFactory.CreateClient(Proxy.ProxyClient);
+            var request = new HttpRequestMessage(new HttpMethod(healthUrl.Method), healthUrl.Url);
+            var response = await client.SendAsync(request);
+            statusCode = response.StatusCode.ToString();
+        }
+        catch (Exception ex)
+        {
+            statusCode = $"\"{ex.Message}\"";
+        }
+
+        if (!Uri.TryCreate(healthUrl.Url, UriKind.Absolute, out var uri))
+            return FormatInvalidUri(healthUrl, statusCode);
+            
+        var hostname = uri.Host;
         var traceRoute = GetTraceRoute(hostname);
         
-        return FormatTraceRoute(healthUrl, response.StatusCode, hostname, traceRoute);
+        return FormatTraceRoute(healthUrl, statusCode, hostname, traceRoute);
     }
 
     private static IEnumerable<string> GetTraceRoute(string hostname)
     {
         const int Timeout = 10000;
-        const int MaxTTL = 30;
+        const int MaxTtl = 30;
         const int BufferSize = 32;
 
         var buffer = new byte[BufferSize];
@@ -38,7 +50,7 @@ public class CheckRoutes(IMessageRoutes messageRoutes, IHttpClientFactory client
         var hopNum = 1;
 
         using var pinger = new Ping();
-        for (var ttl = 1; ttl <= MaxTTL; ttl++)
+        for (var ttl = 1; ttl <= MaxTtl; ttl++)
         {
             var options = new PingOptions(ttl, true);
             
@@ -58,22 +70,33 @@ public class CheckRoutes(IMessageRoutes messageRoutes, IHttpClientFactory client
 
     private static string FormatHopResult(int hopNum, PingReply reply, Stopwatch stopwatch)
     {
-        return $"{hopNum,3}  {GetHostEntry(reply)} [{reply.Address}]  {stopwatch.Elapsed.TotalMilliseconds:0.###} ms";
+        return $"{hopNum,3}  {GetHostEntry(reply.Address.ToString())} [{reply.Address}]  {stopwatch.Elapsed.TotalMilliseconds:0.###} ms";
     }
 
-    private static string FormatTraceRoute(HealthUrl healthUrl, HttpStatusCode statusCode, string hostname, IEnumerable<string> traceRoute)
+    private static string FormatInvalidUri(HealthUrl healthUrl, string statusCode)
     {
         return $@"{healthUrl.Name} - {healthUrl.Method} {healthUrl.Url} - {statusCode}
-traceroute to {hostname} [{GetHostAddresses(hostname)}], 30 hops max
+unable to perform traceroute due to invalid URL
+";
+    }
+
+    private static string FormatTraceRoute(HealthUrl healthUrl, string statusCode, string hostname, IEnumerable<string> traceRoute)
+    {
+        var ipAddresses = GetHostAddresses(hostname);
+        if (ipAddresses.FirstOrDefault() == hostname)
+            hostname = GetHostEntry(hostname);
+        
+        return $@"{healthUrl.Name} - {healthUrl.Method} {healthUrl.Url} - {statusCode}
+traceroute to {hostname} [{string.Join(' ', ipAddresses)}], 30 hops max
 {string.Join("\r", traceRoute)}
 ";
     }
 
-    private static string GetHostEntry(PingReply reply)
+    private static string GetHostEntry(string ipAddress)
     {
         try
         {
-            return Dns.GetHostEntry(reply.Address).HostName;
+            return Dns.GetHostEntry(ipAddress).HostName;
         }
         catch
         {
@@ -81,15 +104,15 @@ traceroute to {hostname} [{GetHostAddresses(hostname)}], 30 hops max
         }
     }
 
-    private static string GetHostAddresses(string hostname)
+    private static string[] GetHostAddresses(string hostname)
     {
         try
         {
-            return string.Join(' ', Dns.GetHostAddresses(hostname).Select(x => x.ToString()));
+            return Dns.GetHostAddresses(hostname).Select(x => x.ToString()).ToArray();
         }
         catch
         {
-            return "*";
+            return ["*"];
         }
     }
 }
