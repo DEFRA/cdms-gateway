f9cd4e51de08217510a073061f5b44a3081e35d5
Ian Shimmings <ian.shimmings@hotmail.com>
2024-11-13 17:15:15 +0000

    Moved trace route from external process to .net code

--- CdmsGateway/Services/Checking/CheckRoutes.cs
+++ CdmsGateway/Services/Checking/CheckRoutes.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
-using System.Runtime.InteropServices;
+using System.Net;
+using System.Net.NetworkInformation;
 using CdmsGateway.Services.Routing;
 using CdmsGateway.Utils.Http;
 
@@ -18,36 +19,77 @@ public class CheckRoutes(IMessageRoutes messageRoutes, IHttpClientFactory client
         var client = clientFactory.CreateClient(Proxy.ProxyClient);
         var request = new HttpRequestMessage(new HttpMethod(healthUrl.Method), healthUrl.Url);
         var response = await client.SendAsync(request);
-        var traceRoute = TraceRoute(healthUrl.Url);
-        return $"{healthUrl.Name} - {healthUrl.Method} {healthUrl.Url} - {response.StatusCode}\r{traceRoute}";
+        
+        var hostname = new Uri(healthUrl.Url).Host;
+        var traceRoute = GetTraceRoute(hostname);
+        
+        return FormatTraceRoute(healthUrl, response.StatusCode, hostname, traceRoute);
     }
 
-    private static string TraceRoute(string url)
+    private static IEnumerable<string> GetTraceRoute(string hostname)
     {
-        var uri = new Uri(url);
-        return RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-            ? RunProcess("cmd.exe", $"/c tracert {uri.Host}")
-                .Replace("\r\n", "\r")
-                .Replace("\rTracing", "Tracing")
-                .Replace("]\rover", "] over")
-                .Replace("\r\r", "\r")
-                .Replace("Trace complete.\r", "")
-            : RunProcess("/bin/bash", $"-c \"traceroute {uri.Host}\"");
-    }
+        const int Timeout = 10000;
+        const int MaxTTL = 30;
+        const int BufferSize = 32;
 
-    private static string RunProcess(string fileName, string arguments)
-    {
-        var startInfo = new ProcessStartInfo()
+        var buffer = new byte[BufferSize];
+        new Random().NextBytes(buffer);
+        var stopwatch = new Stopwatch();
+        var hopNum = 1;
+
+        using var pinger = new Ping();
+        for (var ttl = 1; ttl <= MaxTTL; ttl++)
         {
-            FileName = fileName,
-            Arguments = arguments,
-            RedirectStandardOutput = true,
-            UseShellExecute = false,
-            CreateNoWindow = true
-        };
+            var options = new PingOptions(ttl, true);
             
-        using var process = Process.Start(startInfo);
-        using var reader = process?.StandardOutput;
-        return reader?.ReadToEnd() ?? string.Empty;
+            stopwatch.Restart();
+            var reply = pinger.Send(hostname, Timeout, buffer, options);
+            stopwatch.Stop();
+
+            // we've found a route at this ttl
+            if (reply.Status is IPStatus.Success or IPStatus.TtlExpired)
+                yield return FormatHopResult(hopNum++, reply, stopwatch);
+
+            // if we reach a status other than expired or timed out, we're done searching or there has been an error
+            if (reply.Status != IPStatus.TtlExpired && reply.Status != IPStatus.TimedOut)
+                break;
+        }
+    }
+
+    private static string FormatHopResult(int hopNum, PingReply reply, Stopwatch stopwatch)
+    {
+        return $"{hopNum,3}  {GetHostEntry(reply)} [{reply.Address}]  {stopwatch.Elapsed.TotalMilliseconds:0.###} ms";
+    }
+
+    private static string FormatTraceRoute(HealthUrl healthUrl, HttpStatusCode statusCode, string hostname, IEnumerable<string> traceRoute)
+    {
+        return $@"{healthUrl.Name} - {healthUrl.Method} {healthUrl.Url} - {statusCode}
+traceroute to {hostname} [{GetHostAddresses(hostname)}], 30 hops max
+{string.Join("\r", traceRoute)}
+";
+    }
+
+    private static string GetHostEntry(PingReply reply)
+    {
+        try
+        {
+            return Dns.GetHostEntry(reply.Address).HostName;
+        }
+        catch
+        {
+            return "* * *";
+        }
+    }
+
+    private static string GetHostAddresses(string hostname)
+    {
+        try
+        {
+            return string.Join(' ', Dns.GetHostAddresses(hostname).Select(x => x.ToString()));
+        }
+        catch
+        {
+            return "*";
+        }
     }
 }
