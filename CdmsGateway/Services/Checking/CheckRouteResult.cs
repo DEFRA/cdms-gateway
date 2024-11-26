b5e3a003572c7808946c1cf50c4151129c808715
Ian Shimmings <ian.shimmings@hotmail.com>
2024-11-26 10:15:01 +0000

    Added nslookup to route checking

--- CdmsGateway/Services/Checking/CheckRouteResult.cs
+++ CdmsGateway/Services/Checking/CheckRouteResult.cs
@@ -1,12 +1,3 @@
-using CdmsGateway.Services.Routing;
-
 namespace CdmsGateway.Services.Checking;
 
-public class CheckRouteResult(HealthUrl healthUrl, string responseResult, TimeSpan elapsed)
-{
-    public string RouteName => healthUrl.Name;
-    public string RouteMethod => healthUrl.Method;
-    public string RouteUrl => healthUrl.Url;
-    public string? ResponseResult { get; } = responseResult;
-    public TimeSpan Elapsed { get; } = elapsed;
-}
+public record CheckRouteResult(string RouteName, string RouteUrl, string CheckType, string ResponseResult, TimeSpan Elapsed);
